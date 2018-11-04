25a74ad272ed1c6fbb9c6cb22ab9ef568610e640
Ira <irina.kalmykova83@gmail.com>
2018-11-05 01:30:55 +0200

    finish

--- script/script.js
+++ script/script.js
@@ -13,13 +13,32 @@ $(document).ready(function() {
     $('.slick-slider-2').slick({
         dots: false,
         infinite: true,
-        speed: 400,
-        // centerMode: true,
+        speed: 300,
+        centerMode: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-        // autoplay:true,
-        // fade: true
+        autoplay:true,
+        fade: true
     });
+
+    $('.slick-slider-3').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        prevArrow:'<img class="prev" src="../img/prev.svg">',
+        nextArrow:'<img class="next" src="../img/next.svg">'
+    });
+
+    (function() {
+
+        var swInstance = new ScrollWatch({
+            watchOnce: false
+        });
+
+    })();
 
 });
